6abbac934e41104a8735a923573eebb07226b243
Craftznake <dev.craftznake@gmail.com>
2024-08-10 10:50:45 +0700

    nvim: update config

--- .config/nvim/lua/craftznake/lazy/undotree.lua
+++ .config/nvim/lua/craftznake/lazy/undotree.lua
@@ -1,7 +1,15 @@
 return {
   "mbbill/undotree",
-
+  dependencies = {
+    "folke/which-key.nvim",
+  },
   config = function()
-    vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "[T]oggle [U]undo tree" })
+    local wk = require("which-key")
+    wk.add({
+      {
+        mode = { "n" },
+        { "<leader>tu", vim.cmd.UndotreeToggle, desc = "[U]ndo Tree", noremap = true, silent = true },
+      },
+    })
   end,
 }
