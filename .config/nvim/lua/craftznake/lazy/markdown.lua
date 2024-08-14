2daa52c8ac3fc887a26986ba3e4165e0a84e1769
Craftznake <minhdat15012002@gmail.com>
2024-08-14 09:14:39 +0700

    update nvim, use another preview markdown, config dap for python

--- .config/nvim/lua/craftznake/lazy/markdown.lua
+++ .config/nvim/lua/craftznake/lazy/markdown.lua
@@ -16,10 +16,17 @@ return {
     end,
   },
   {
-    "MeanderingProgrammer/markdown.nvim",
-    main = "render-markdown",
-    opts = {},
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim", "nvim-tree/nvim-web-devicons" }, -- if you use the mini.nvim suite
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
   },
 }
