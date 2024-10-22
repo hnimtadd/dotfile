aa31227742789f560447576849f56003f70e760e
Craftznake <dev.craftznake@gmail.com>
2024-10-22 16:48:09 +0700

    testing with zsh

---
+++ .config/zsh/craftznake/plugins.before/zsh-eza.zsh
@@ -0,0 +1,3 @@
+AUTOCD=1
+_EZA_PARAMS=('--git' '--group' '--group-directories-first' '--time-style=long-iso' '--color-scale=all' '--icons' '--color-scale-mode=gradient')
+export EZA_CONFIG_DIR=$HOME/.config/eza
