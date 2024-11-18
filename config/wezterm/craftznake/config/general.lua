90ed47177c6afa9b6ceb0b64f356fd1217b58c5f
Craftznake <minhdat15012002@gmail.com>
2024-11-18 11:05:07 +0700

    wezterm: change exit behavior to exit

--- config/wezterm/craftznake/config/general.lua
+++ config/wezterm/craftznake/config/general.lua
@@ -1,7 +1,7 @@
 return {
 	-- behaviours
 	automatically_reload_config = true,
-	exit_behavior = "CloseOnCleanExit", -- if the shell program exited with a successful status
+	exit_behavior = "Close", -- if the shell program exited with a successful status
 	exit_behavior_messaging = "Verbose",
 	status_update_interval = 1000,
 	enable_csi_u_key_encoding = true,
