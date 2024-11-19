2a498b8b692bfd2c7f3d034a2f868bc22f6b593f
Craftznake <minhdat15012002@gmail.com>
2024-11-19 23:43:29 +0700

    refine configuration

--- config/nix-darwin/modules/darwin/apps.nix
+++ config/nix-darwin/modules/darwin/apps.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
-# let
-#     yabai = pkgs.yabai.overrideAttrs (old: rec {
-#       src = pkgs.fetchFromGitHub {
-#         owner = "donaldguy";
-#         repo = "yabai";
-#         rev = "723f2ee0346b78360c497b7a7eb7be409c02302c";
-#         sha256 = "sha256:1z95njalhvyfs2xx6d91p9b013pc4ad846drhw0k5gipvl03pp92";
-#       };
-#     });
-# in
 {
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
@@ -18,7 +8,6 @@
   ];
   homebrew = {
     enable = true;
-
     onActivation = {
       autoUpdate = false;
       cleanup = "zap";
