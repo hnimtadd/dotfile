488bc1e5e760e8d4ae80b10c3bf91763e89d7051
Craftznake <minhdat15012002@gmail.com>
2024-12-10 22:10:48 +0700

    wezterm: config right part of status bar

--- config/wezterm/craftznake/config/appearance.lua
+++ config/wezterm/craftznake/config/appearance.lua
@@ -1,8 +1,5 @@
 local wezterm = require("wezterm")
 
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
 local process_icons = {
 	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
 	["btm"] = wezterm.nerdfonts.mdi_chart_donut_variant,
@@ -122,20 +119,6 @@ local function select_contrasting_fg_color(hex_color)
 	end
 	return "#FFFFFF" -- White has higher contrast
 end
--- This function returns the suggested title for a tab
--- It prefers the title that was set via tab:set_title() or wezterm cli set-tab-title,
--- but falls back to the title of the active pane in that tab
-local function tab_title(tab_info, max_width)
-	local title = tab_info.tab_title
-	if title and #title > 0 then
-		return title
-	end
-	if max_width == 0 then
-		return tab_info.tab_index .. ": " .. tab_info.active_pane.title
-	else
-		return tab_info.tab_index .. ": " .. wezterm.truncate_left(tab_info.active_pane.title, max_width - 2)
-	end
-end
 
 -- On format tab title events, override the default handling to return a custom title
 -- Docs: https://wezfurlong.org/wezterm/config/lua/window-events/format-tab-title.html
@@ -159,6 +142,106 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 		}
 	end
 	return title
+end)
+
+-- update right status bar
+-- Docs: https://wezfurlong.org/wezterm/config/lua/window-events/update-right-status.html
+-- @diagnostic display-next-line: unused-local
+wezterm.on("update-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
+
+	-- Figure out the cwd and host of the current pane.
+	-- This will pick up the hostname for the remote host if your
+	-- shell is using OSC 7 on the remote host.
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		local cwd = ""
+		local hostname = ""
+
+		if type(cwd_uri) == "userdata" then
+			-- Running on a newer version of wezterm and we have
+			-- a URL object here, making this simple!
+
+			cwd = cwd_uri.file_path
+			hostname = cwd_uri.host or wezterm.hostname()
+		else
+			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+			-- which doesn't have the Url object
+			cwd_uri = cwd_uri:sub(8)
+			local slash = cwd_uri:find("/")
+			if slash then
+				hostname = cwd_uri:sub(1, slash - 1)
+				-- and extract the cwd from the uri, decoding %-encoding
+				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+					return string.char(tonumber(hex, 16))
+				end)
+			end
+		end
+
+		-- Remove the domain name portion of the hostname
+		local dot = hostname:find("[.]")
+		if dot then
+			hostname = hostname:sub(1, dot - 1)
+		end
+		if hostname == "" then
+			hostname = wezterm.hostname()
+		end
+
+		table.insert(cells, cwd)
+		table.insert(cells, hostname)
+	end
+
+	-- I like my date/time in this style: "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M")
+	table.insert(cells, date)
+
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+
+	-- The powerline < symbol
+	local LEFT_ARROW = utf8.char(0xe0b3)
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	function push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
 end)
 
 return {
