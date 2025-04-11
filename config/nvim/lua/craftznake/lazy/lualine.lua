88ca61de73d12dd50c398f762597d1ba76fb5e1b
Craftznake <minhdat15012002@gmail.com>
2025-04-11 20:39:06 +0700

    nvim: rework neovim with classic ui

--- config/nvim/lua/craftznake/lazy/lualine.lua
+++ config/nvim/lua/craftznake/lazy/lualine.lua
@@ -1,58 +1,30 @@
 return {
   {
     "letieu/harpoon-lualine",
-    dependencies = {
-      {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-      },
-    },
   },
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- You can use a custom component extending builtin filename component to do this.
-
-      local custom_fname = require("lualine.components.filename"):extend()
-      local highlight = require("lualine.highlight")
-      local default_status_colors = { saved = "#228B22", modified = "#C70039" }
-
-      function custom_fname:init(options)
-        custom_fname.super.init(self, options)
-        self.status_colors = {
-          saved = highlight.create_component_highlight_group({}, "filename_status_saved", self.options),
-          modified = highlight.create_component_highlight_group(
-            { fg = default_status_colors.modified },
-            "filename_status_modified",
-            self.options
-          ),
-        }
-        if self.options.color == nil then
-          self.options.color = ""
-        end
+      local function location()
+        return string.format("%3d:%-2d", unpack(vim.api.nvim_win_get_cursor(0)))
       end
 
-      function custom_fname:update_status()
-        local data = custom_fname.super.update_status(self)
-        data = highlight.component_format_highlight(
-          vim.bo.modified and self.status_colors.modified or self.status_colors.saved
-        ) .. data
-        return data
-      end
+      local themes = require("craftznake.lazy.consts.themes")
 
       require("lualine").setup({
         options = {
           icons_enabled = true,
-          section_separators = { left = "", right = "" },
-          component_separators = { left = "⟫", right = "⟪" },
-          -- component_separators = { left = "⟩", right = "⟨⟮" },
+          section_separators = "",
+          component_separators = "",
+          theme = themes.nil_theme,
+          always_divide_middle = true,
           disabled_filetypes = {
             statusline = {},
             winbar = {},
           },
           ignore_focus = {},
-          always_divide_middle = false,
           globalstatus = false,
           refresh = {
             statusline = 1000,
@@ -64,48 +36,50 @@ return {
           lualine_a = {
             {
               "mode",
+              fmt = function(mode)
+                return mode:sub(1, 1)
+              end,
             },
           },
-          lualine_b = {},
-          lualine_c = {
-            custom_fname,
-          },
-          lualine_x = {
+          lualine_b = {
+            -- custom_fname,
             {
-              "harpoon2",
-              icon = "♥",
-              indicators = { "1", "2", "3", "4" },
-              active_indicators = { "[1]", "[2]", "[3]", "[4]" },
-              _separator = " ",
-              no_harpoon = "Harpoon not loaded",
-            },
-            {
-              "encoding",
-            },
-            {
-              "fileformat",
+              "filename",
+              file_status = true,
+              path = 0,
               symbols = {
-                unix = "", -- e712
-                dos = "", -- e70f
-                mac = "", -- e711
+                modified = "+",
+                readonly = "-",
+                unnamed = "[No Name]",
               },
             },
+          },
+          lualine_c = {
             {
-              "filetype",
-              colored = true, -- Displays filetype icon in color if set to true
-              icon_only = false, -- Display only an icon for filetype
+              "diagnostics",
+              symbols = {
+                error = "E",
+                warn = "W",
+                info = "I",
+                hint = "H",
+              },
             },
+          },
+          lualine_x = {
+            "encoding",
+            "fileformat",
+            "filetype",
           },
           lualine_y = {
             "progress",
           },
-          lualine_z = { "location" },
+          lualine_z = { location },
         },
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
           lualine_c = { "filename" },
-          lualine_x = { "location" },
+          lualine_x = { location },
           lualine_y = {},
           lualine_z = {},
         },
--- config/nvim/lua/craftznake/set.lua
+++ config/nvim/lua/craftznake/set.lua
@@ -22,7 +22,7 @@ vim.opt.undofile = true
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8"
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
