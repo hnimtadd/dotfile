3c32fbb8ac0aa3f90e245137efff2fc18b60faa6
Hnimtadd <minhdat15012002@gmail.com>
2025-05-18 17:29:51 +0700

    nvim: re-enable go for debugging, remove unused gpt plugin

--- config/nvim/lua/craftznake/plugins/init.lua
+++ config/nvim/lua/craftznake/plugins/init.lua
@@ -11,5 +11,5 @@ return {
     { "folke/lazy.nvim",                                     version = false },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    -- { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.go" },
 }
